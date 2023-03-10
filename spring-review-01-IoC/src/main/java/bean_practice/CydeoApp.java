9dee37d58ead3167e8208d789a5efde68eb3e507
Maxim <maxbilenkin@gmail.com>
2023-03-10 00:19:38 -0500

    task-1 is done

---
+++ spring-review-01-IoC/src/main/java/bean_practice/CydeoApp.java
@@ -0,0 +1,18 @@
+package bean_practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        //how to create spring container ?
+        ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        FullTimeEmployee ft = container.getBean(FullTimeEmployee.class);
+        ft.createAccount();
+
+        PartTimeEmployee pt = container.getBean(PartTimeEmployee.class);
+        pt.createAccount();
+    }
+}
